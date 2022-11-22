c477f155a9c1c97de2d7dcd57495edf80e36a6cb
Jason <108306087@g.nccu.edu.tw>
2022-11-23 07:03:08 +0800

    搞定 remake。

--- community/views.py
+++ community/views.py
@@ -218,88 +218,75 @@ def remake_outfit(request, postPk):
 
 # 復刻穿搭選擇頁面 (select_remake)
 def select_remake_outfit(request, postPk):
-    # user = request.user
-    # post = Post.objects.get(id=postPk)
-    # user_closets = Closet.objects.filter(user_id=user.id)
-    # model = Clothe.objects.filter(user=user).first()
-    # path = Path(model.image.path)
-    # p = str(path.absolute())
-    # findsimilar.selectarea(p)
-
+    # refresh model first.
     user = request.user
     post = Post.objects.get(id=postPk)
-    if request.POST.get('refresh', None): # refresh similarity model.
-        refresh = True
-        user_clothes = request.user.clothe_set.all()
-        dir_path = settings.BASE_DIR / Path('media', 'images', 'remake', str(user.id))
-        if not dir_path.exists():
-            dir_path.mkdir(parents=True, exist_ok=True)
+    user_clothes = request.user.clothe_set.all()
+    dir_path = settings.BASE_DIR / Path('media', 'images', 'remake', str(user.id))
+    if not dir_path.exists():
+        dir_path.mkdir(parents=True, exist_ok=True)
 
-        # copy all images of user's clothes to `dir_path`.
-        for clothe in user_clothes:
-            new_filename = f'{user.id}_{clothe.id}.' + clothe.image.path.split('.')[-1]
-            shutil.copyfile(clothe.image.path, dir_path / new_filename)
+    # copy all images of user's clothes to `dir_path`.
+    for clothe in user_clothes:
+        new_filename = f'{user.id}_{clothe.id}.' + clothe.image.path.split('.')[-1]
+        shutil.copyfile(clothe.image.path, dir_path / new_filename)
 
-        path = dir_path
-
-    else: # remake outfit.
-        refresh = False
-        # get the original image.
-        orig_img_path = post.image.path
-
-        # process the list-like string to really list: regions.
-        regions = []
-        for key, value in request.POST.items():
-            if key.startswith('region'):
-                regions.append(
-                    [int(each) for each in ''.join([i for i in value if i not in '[]']).split(',')]
-                )
-        
-        # cut the selected regions.
-        orig_img = cv2.imread(orig_img_path)
-        
-        # conversion of the image size between on the webpage and the original image.
-        web_img_height = int(request.POST.get('img_height'))
-        img_size_ratio = orig_img.shape[0] / web_img_height
-
-        img_paths = []
-        for idx, (x, y, w, h) in enumerate(regions):
-            x, y, w, h = [int(i * img_size_ratio) for i in (x, y, w, h)]
-            img = orig_img[y: y + h, x: x + w]
-            # set the path to save the cutted image.
-            filename = hashlib.md5(f'{user.id}_{orig_img_path}_{idx}'.encode()).hexdigest() + '.jpg'
-            path = str(settings.BASE_DIR / Path('media', 'images', 'remake', filename))
-            cv2.imwrite(path, img)
-            img_paths.append(path)
-
-        # Somethings that maybe concerned:
-        # 1. how to cut the clothe image from the outfit image.
-        # 2. from the above line, save the result of cutting image.
-        # that all i have thought.
-
+    path = dir_path
 
     uid = user.id
     model_dir = settings.BASE_DIR / Path('ai_models', 'similarity_models')
-    if refresh:
-       command = f'python {Path(settings.BASE_DIR, "ai_models/findsimilar.py")} {uid} {path} {model_dir} --refresh=1'
-       result = os.system(command)
-    else: # select regions.
-        results = []
-        for path in img_paths:
-            command = f'python {Path(settings.BASE_DIR, "ai_models/findsimilar.py")} {uid} {path} {model_dir}'
-            result = os.popen(command).read()
-            result = result.split('model result: ')[-1]
-            result = [int(each) for each in ''.join([i for i in result if i not in '()']).split(', ')] # result = [user_id, clothe_id]
-            results.append(result)
+    command = f'python {Path(settings.BASE_DIR, "ai_models/findsimilar.py")} {uid} {path} {model_dir} --refresh=1'
+    result = os.system(command)
+    shutil.rmtree(dir_path)
 
-    if refresh: # remove all the cutted images.
-        shutil.rmtree(dir_path)
-    else:
-        for img in img_paths:
-            os.remove(img)
 
-    if refresh:
-        return redirect(reverse('remake_outfit', kwargs={'postPk': post.id}))
+    # then, remake outfit.
+    # get the original image.
+    orig_img_path = post.image.path
+
+    # process the list-like string to really list: regions.
+    regions = []
+    for key, value in request.POST.items():
+        if key.startswith('region'):
+            regions.append(
+                [int(each) for each in ''.join([i for i in value if i not in '[]']).split(',')]
+            )
+    
+    # cut the selected regions.
+    orig_img = cv2.imread(orig_img_path)
+    
+    # conversion of the image size between on the webpage and the original image.
+    web_img_height = int(request.POST.get('img_height'))
+    img_size_ratio = orig_img.shape[0] / web_img_height
+
+    img_paths = []
+    for idx, (x, y, w, h) in enumerate(regions):
+        x, y, w, h = [int(i * img_size_ratio) for i in (x, y, w, h)]
+        img = orig_img[y: y + h, x: x + w]
+        # set the path to save the cutted image.
+        filename = hashlib.md5(f'{user.id}_{orig_img_path}_{idx}'.encode()).hexdigest() + '.jpg'
+        path = str(settings.BASE_DIR / Path('media', 'images', 'remake', filename))
+        cv2.imwrite(path, img)
+        img_paths.append(path)
+
+    # Somethings that maybe concerned:
+    # 1. how to cut the clothe image from the outfit image.
+    # 2. from the above line, save the result of cutting image.
+    # that all i have thought.
+
+    uid = user.id
+    model_dir = settings.BASE_DIR / Path('ai_models', 'similarity_models')
+    # select regions.
+    results = []
+    for path in img_paths:
+        command = f'python {Path(settings.BASE_DIR, "ai_models/findsimilar.py")} {uid} {path} {model_dir}'
+        result = os.popen(command).read()
+        result = result.split('model result: ')[-1]
+        result = [int(each) for each in ''.join([i for i in result if i not in '()']).split(', ')] # result = [user_id, clothe_id]
+        results.append(result)
+
+    for img in img_paths:
+        os.remove(img)
 
     user_closets = user.closets
     clothes = [Clothe.objects.get(id=result[-1]) for result in results]
